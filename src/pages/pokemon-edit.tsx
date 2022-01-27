507a41c833ab6e8fa4cdd24d96d7ed5e45de79a3
IamZack <72028548+IGDERZEN-Zakaria@users.noreply.github.com>
2022-01-27 19:18:34 +0100

    Update pokemon-edit.tsx

--- src/pages/pokemon-edit.tsx
+++ src/pages/pokemon-edit.tsx
@@ -3,6 +3,8 @@ import { RouteComponentProps } from "react-router-dom";
 import PokemonForm from "../components/pokemon-form";
 import Pokemon from "../models/pokemon";
 import POKEMONS from "../models/mock-pokemon";
+import PokemonService from "../services/pokemon-service";
+import Loader from "../components/loader";
 
 type Params = { id: string };
 
@@ -21,7 +23,9 @@ const PokemonEdit: FunctionComponent<RouteComponentProps<Params>> = ({ match }) 
           <PokemonForm pokemon={pokemon} isEditForm={true}></PokemonForm>
         </div>
       ) : (
-        <h4 className="center">Aucun pokémon à afficher !</h4>
+        <h4 className="center">
+          <Loader />
+        </h4>
       )}
     </div>
   );
