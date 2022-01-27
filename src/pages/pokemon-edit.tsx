df2dea7f715b2ab18ffd8c135683237fbe99ce7e
IGDERZEN-Zakaria <zakaria.igderzen@gmail.com>
2022-01-27 21:09:07 +0100

    15-deployment

--- src/pages/pokemon-edit.tsx
+++ src/pages/pokemon-edit.tsx
@@ -3,6 +3,8 @@ import { RouteComponentProps } from "react-router-dom";
 import PokemonForm from "../components/pokemon-form";
 import Pokemon from "../models/pokemon";
 import POKEMONS from "../models/mock-pokemon";
+import PokemonService from "../services/pokemon-service";
+import Loader from "../components/loader";
 
 type Params = { id: string };
 
@@ -10,11 +12,7 @@ const PokemonEdit: FunctionComponent<RouteComponentProps<Params>> = ({ match }) 
   const [pokemon, setPokemon] = useState<Pokemon | null>(null);
 
   useEffect(() => {
-    POKEMONS.forEach((pokemon) => {
-      if (match.params.id === pokemon.id.toString()) {
-        setPokemon(pokemon);
-      }
-    });
+    PokemonService.getPokemon(+match.params.id).then((pokemon) => setPokemon(pokemon));
   }, [match.params.id]);
 
   return (
@@ -25,7 +23,9 @@ const PokemonEdit: FunctionComponent<RouteComponentProps<Params>> = ({ match }) 
           <PokemonForm pokemon={pokemon} isEditForm={true}></PokemonForm>
         </div>
       ) : (
-        <h4 className="center">Aucun pokémon à afficher !</h4>
+        <h4 className="center">
+          <Loader />
+        </h4>
       )}
     </div>
   );
